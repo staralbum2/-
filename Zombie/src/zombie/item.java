803a27429423c51ecf6a97fb4a724efbb492fa19
tj <staralbum2@gmail.com>
2024-11-11 15:18:44 +0900

    create item

--- Zombie/src/zombie/item.java
+++ Zombie/src/zombie/item.java
@@ -1,5 +1,14 @@
 package zombie;
 
 public class item {
-
+	private int power;
+	private int defence;
+	private int hp;
+	
+	item(int power,int defence,int hp){
+		this.power = power;
+		this.defence = defence;
+		this.hp = hp;
+		
+	}
 }
