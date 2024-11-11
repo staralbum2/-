765ffac61266346298d995ce906885e94d8c6a62
tj <staralbum2@gmail.com>
2024-11-11 18:21:36 +0900

    update recovery()

--- Zombie/src/zombie/Hero.java
+++ Zombie/src/zombie/Hero.java
@@ -5,7 +5,7 @@ public class Hero extends Units{
 		super(30,3,10,150,name,location);
 	}
 	public void recovery() {
-			setHp(getHp() + 100);
+			setHp(getHp() + 50);
 			System.out.println("체력 회복해서" + getHp() + "이 되었습니다");
 	}
 	@Override
