e6eb88ebfbe60a8a258b1510a6eb75d2d1820102
tj <staralbum2@gmail.com>
2024-11-11 15:16:36 +0900

    update units

--- Zombie/src/zombie/Units.java
+++ Zombie/src/zombie/Units.java
@@ -1,12 +1,50 @@
 package zombie;
 
-public class Units {
+import java.util.Random;
+
+abstract public class Units {
 	private int power;
 	private int defense;
 	private int critical;
+	private int hp;
+	private Random ran;
 	
 	
-	Units(){
-		
+	
+	Units(int power,int defense,int critical,int hp){
+		this.power = power;
+		this.defense = defense;
+		this.critical = critical;
+		this.hp = hp;
+		ran = new Random();
 	}
+	
+	public int getPower() {
+		return this.power;
+	}
+	public void setPower(int power) {
+		this.power = power;
+	}
+	public int getDefense() {
+		return this.defense;
+	}
+	public void setDefense(int defense) {
+		this.defense = defense;
+	}
+	public int getCritical() {
+		return this.critical;
+	}
+	public void setCritical(int critical) {
+		this.critical = critical;
+	}
+	public int getHp() {
+		return this.hp;
+	}
+	public void setHp(int hp) {
+		this.hp = hp;
+	}
+	
+	
+	abstract void attack(Units unit);	
+	
 }
