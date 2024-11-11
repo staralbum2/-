bbed6dc7958cb2d561e416c375c517c06d3f9a69
tj <staralbum2@gmail.com>
2024-11-11 17:14:48 +0900

    update setGame

--- Zombie/src/zombie/ZombieGame.java
+++ Zombie/src/zombie/ZombieGame.java
@@ -1,17 +1,26 @@
 package zombie;
 
+import java.util.Random;
+
 public class ZombieGame {
+	
+	private Random ran = new Random();
 	
 	private Hero hero;
 	private ZombieKing zombieKing;
 	private ZombieKnight zombieKnight;
 	private ZombieSoldier zombieSoldier;
+	private int loca = 1;
 	
 	public void run() {
 		setGame();
 	}
 	
 	private void setGame() {
-		
+		hero.setLocation(loca);
+		zombieKing = new ZombieKing();
+		zombieKnight = new ZombieKnight();
+		int soldierLocation = ran.nextInt(6)+3;
+		zombieSoldier = new ZombieSoldier(soldierLocation);
 	}
 }
