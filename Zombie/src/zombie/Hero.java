3e5c37ed4dceb52a4604063c0bf79e23df22910f
tj <staralbum2@gmail.com>
2024-11-12 12:11:47 +0900

    update inventori

--- Zombie/src/zombie/Hero.java
+++ Zombie/src/zombie/Hero.java
@@ -1,30 +1,34 @@
 package zombie;
 
-public class Hero extends Units{
+public class Hero extends Units {
 	private Item inventori[] = new Item[10];
-	Hero(String name,int location){
-		super(30,3,10,150,name,location);
+
+	Hero(String name, int location) {
+		super(30, 3, 10, 150, name, location);
 	}
+
 	public void recovery() {
-			setHp(getHp() + 50);
-			System.out.println("체력 회복해서" + getHp() + "이 되었습니다");
+		setHp(getHp() + 50);
+		System.out.println("체력 회복해서" + getHp() + "이 되었습니다");
 	}
-	
+	public Item[] getInventori() {
+		return inventori;
+	}
 	@Override
 	public String toString() {
 		String info = "";
-		for(int i=0;i<inventori.length;i++) {
-			if(inventori[i] == null) 
-				info += String.format("%d) no item\n",i+1);			
-			else 
-				info +=String.format("%d) %s\n",i+1,inventori[i].getName());		
+		for (int i = 0; i < inventori.length; i++) {
+			if (inventori[i] == null)
+				info += String.format("%d) no item\n", i + 1);
+			else
+				info += String.format("%d) %s\n", i + 1, inventori[i].getName());
 		}
 		return info;
 	}
-	
+
 	@Override
-	void attack(Units unit) {
-		// TODO Auto-generated method stub
+	void attack(Units enemy) {
 		
 	}
+
 }
