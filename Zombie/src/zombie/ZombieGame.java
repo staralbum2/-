52c395981d669d035009046d655d00f255001c95
tj <staralbum2@gmail.com>
2024-11-12 12:47:30 +0900

    create inventori

--- Zombie/src/zombie/ZombieGame.java
+++ Zombie/src/zombie/ZombieGame.java
@@ -29,13 +29,19 @@ public class ZombieGame {
 		zombieKnight = new ZombieKnight();
 		soldierloca = ran.nextInt(6) + 3;
 		zombieSoldier = new ZombieSoldier(soldierloca);
-
+		
 	}
-
+	private void setEquipment() {
+		System.out.println(hero);
+		int sel = inputNum("착용할 장비 번호")-1;
+		Item[] inventori = hero.getInventori();
+		
+	}
+	
 	private String setHeroName() {
 		String name = "";
 		while (true) {
-			name = inputString("용사의 이름을 알려주세요\n");
+			name = inputString("용사의 이름을 알려주세요");
 			System.out.println("용사의 이름이" + name + "가 맞습니까?");
 			int sel = inputNum("1)맞아\n2)아니야\n이름 입력");
 			if (sel == 1) {
@@ -48,21 +54,23 @@ public class ZombieGame {
 
 	private void play() {
 		System.out.println("현재 위치 = " + loca);
-		int move = inputNum("앞으로 이동하기(1),휴식하기(2)");
+		int move = inputNum("앞으로 이동하기(1),휴식하기(2),인벤토리 열기(3)");
 
 		if (move == 1) {
 			loca = loca + 1;
-			hero.setLocation(loca);
+			hero.setLocation(loca); 
 
 			if (hero.getLocation() == 5) 
 				battle(zombieSoldier);
-			
 			if (hero.getLocation() == 10) 
 				battle(zombieKnight);
 			if (hero.getLocation() == 14) 
 				battle(zombieKing);		
 		} else if (move == 2) {
 			hero.recovery();
+		}
+		else if(move == 3) {
+			showInventori();
 		}
 	}
 	private void battle(Units enemy) {
@@ -71,25 +79,37 @@ public class ZombieGame {
 			int sel = inputNum("공격하기(1),회복마법(2): ");
 			if (sel == 1) {
 				enemy.attack(hero);
-
 				hero.attack(enemy);
 			}
-
 			else if (sel == 2)
 				hero.recovery();
-
+			
 			if (hero.getHp() <= 0) {
 				System.out.printf("%s가 죽었습니다. 게임오버!", hero.getName());
 				break;
 			}
-
 			if (enemy.getHp() <= 0) {
-				System.out.printf("%s 격파!",enemy);
+				System.out.printf("%s 격파!",enemy.getName());
 				break;
 			}
 		}
 	}
-
+	
+	private void showInventori() {
+		System.out.println(hero);
+	}
+	private void dropItem() {
+		int dice = ran.nextInt(999);
+		
+		//0~2
+		//처음 꺼는 드랍률 50퍼정도로
+		// 안뜨면 확률 25퍼증가
+		// 뜨면 -
+		// 1or 2로 해서
+		// 웨펀 아머 구분
+		// 1이면 1번 2면 2번 반복해서 드랍아이템
+		// 
+	}
 	private int inputNum(String msg) {
 		int num = -1;
 		while (true) {
@@ -102,7 +122,7 @@ public class ZombieGame {
 			}
 			if (num != -1)
 				break;
-			System.out.println("잘못된 숫자를 입력했습니다.");
+			
 		}
 		return num;
 	}
